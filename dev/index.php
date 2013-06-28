425c23b863f91dbff20ebe7f6b22b92261551598
Aleksei <264762@mail.ru>
2013-06-28 02:03:21 -0700

    FIX: Ослик больше не выпендривается.

--- dev/index.php
+++ dev/index.php
@@ -9,6 +9,17 @@
 <script type="text/javascript" src="<?=__URL__;?>dev/jqmodalwin-1.2.js"></script>
 </head>
 <body>
+<style>
+	
+	html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {margin: 0; padding: 0; border: 0; vertical-align: baseline;}
+	
+	/* A */
+ 
+	html {width:100%; height:100%;}
+ * {font-size: 14px; font-family: "Helvetica Neue", Arial, Tahoma !important; line-height: 1.4; }
+	body {padding:0;margin:0;height:100%;background-color: #fafafa; text-align: center;}
+	
+</style>
 <?
 //require("temp.php");
 ?>
